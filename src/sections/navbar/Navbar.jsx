9ca37a350e7d1493eb920c1fed7453732314721c
steph-ayo <popoolastephen19@gmail.com>
2024-07-06 15:31:41 +0100

    navbar update

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -2,23 +2,37 @@ import logo from "../../assets/images/logo.svg";
 import data from "./data";
 import "./navbar.css";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { LiaTimesSolid } from "react-icons/lia"; // Import the close icon
+import { useState } from "react";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <nav>
       <div className="container nav-container">
         <a href="index.html" className="nav-logo">
-          <img src={logo} alt="logo"></img>
+          <img src={logo} alt="logo" />
         </a>
-        <ul className="nav-menu">
+        <ul className={`nav-menu ${menuOpen ? "show" : ""}`}>
           {data.map((item) => (
             <li key={item.id}>
-              <a href={item.link}>{item.title}</a>
+              <a href={item.link} onClick={closeMenu}>
+                {item.title}
+              </a>
             </li>
           ))}
         </ul>
-        <button id="nav-burger" onClick="#">
-          <RxHamburgerMenu />
+        <button id="nav-burger" onClick={toggleMenu}>
+          {menuOpen ? <LiaTimesSolid /> : <RxHamburgerMenu />}
         </button>
       </div>
     </nav>
