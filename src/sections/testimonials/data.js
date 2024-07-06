6877c7d04e6ac2854cc53757a4a258b4a62b9bd2
steph-ayo <popoolastephen19@gmail.com>
2024-07-06 16:04:10 +0100

    testimonial update

--- src/sections/testimonials/data.js
+++ src/sections/testimonials/data.js
@@ -1,6 +1,7 @@
-import Babara from "../../assets/testimonials/Babara.png";
-import Mfm from "../../assets/testimonials/mfm.png";
-import Daramola from "../../assets/testimonials/Daramola.png";
+import Babara from "../../assets/images/testimonials/Babara.png";
+import Mfm from "../../assets/images/testimonials/mfm.png";
+import Daramola from "../../assets/images/testimonials/Daramola.png";
+import Johnson from "../../assets/images/testimonials/Johnson.jpg";
 
 const data = [
   {
@@ -9,7 +10,7 @@ const data = [
       '"The instructors at Evans vocals are knowledgeable and patient... I gained invaluable skills in both music theory and practical application, which have significantly improved my playing and composition abilities."',
     avatar: Babara,
     name: "Babaraoluwa",
-    profession: "Student class of 2023",
+    profession: "Student",
   },
   {
     id: 2,
@@ -30,26 +31,18 @@ const data = [
   {
     id: 4,
     quote:
-      "“I highly recommend Etim Vocals Enterprises! From the moment we engaged their services, it was clear we were in the hands of true professionals. Their team’s expertise and passion for music production shone through in every interaction.”",
-    avatar: Daramola,
-    name: "Daramola",
-    profession: "Customer",
+      "“Etim Vocals Enterprises has been instrumental in helping our choir reach new heights. Their guidance and support have been invaluable, and their commitment to excellence is unmatched.”",
+    avatar: Johnson,
+    name: "Johnson Adeyemi",
+    profession: "Choir Director",
   },
   {
     id: 5,
     quote:
-      "“I highly recommend Etim Vocals Enterprises! From the moment we engaged their services, it was clear we were in the hands of true professionals. Their team’s expertise and passion for music production shone through in every interaction.”",
-    avatar: Daramola,
-    name: "Daramola",
-    profession: "Customer",
-  },
-  {
-    id: 6,
-    quote:
-      "“I highly recommend Etim Vocals Enterprises! From the moment we engaged their services, it was clear we were in the hands of true professionals. Their team’s expertise and passion for music production shone through in every interaction.”",
-    avatar: Daramola,
-    name: "Daramola",
-    profession: "Customer",
+      "“Working with Etim Vocals Enterprises was a game-changer for our music project. Their expertise and creative input made all the difference, and the final product exceeded our expectations.”",
+    avatar: Johnson,
+    name: "Alex P.",
+    profession: "Music Producer",
   },
 ];
 
